66426b7fb0fa9ad4283a09efd7b4ab6399917c67
Kumar McMillan <kumar.mcmillan@gmail.com>
2015-11-14 21:03:50 -0600

    removed unused app action

--- public/js/lib/actions/app.js
+++ public/js/lib/actions/app.js
@@ -21,24 +21,3 @@ export function resetState() {
     });
   };
 }
-
-
-export function showStatus({apiFetch=fetch} = {}) {
-  return (dispatch) => apiFetch('http://olympia.dev:8000/',
-                                {headers: {accept: 'application/json'}})
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      dispatch({
-        type: actionTypes.SET_STATUS,
-        status: data.status,
-      });
-    })
-    .catch((error) => {
-      dispatch({
-        type: actionTypes.APP_ERROR,
-        error: 'Could not fetch status: ' + error,
-      });
-    });
-}
--- public/js/lib/reducers/app.js
+++ public/js/lib/reducers/app.js
@@ -3,7 +3,6 @@ import * as actionTypes from 'lib/constants/action-types';
 
 export const initialAppState = {
   error: null,
-  status: null,
 };
 
 
@@ -16,10 +15,6 @@ export default function app(state, action) {
     case actionTypes.APP_ERROR:
       return Object.assign({}, state, {
         error: action.error,
-      });
-    case actionTypes.SET_STATUS:
-      return Object.assign({}, state, {
-        status: action.status,
       });
     default:
       return state || initialAppState;
